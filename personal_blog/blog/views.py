c34d271e61607e6d303d642f272721d3b6b91692
Daniil669 <daniil.freeman669@gmail.com>
2026-01-11 10:50:24 +0200

    one more view, few custom tags and templates

--- personal_blog/blog/views.py
+++ personal_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,8 @@ def index(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {'post': post}
+    return render(request, 'blog/post-detail.html', context)
